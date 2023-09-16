b632e5adb9f668e2d25153e0e9e765a8686eed9e
Neko-Turu <kirmansyah9696@gmail.com>
2023-09-16 13:49:07 +0000

    Revert "Update required KernelSU"
    This reverts commit 8053864f28bb6d28e0c0433685a4d533d8bbb102.

--- fs/exec.c
+++ fs/exec.c
@@ -1713,16 +1713,9 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
-#ifdef CONFIG_KSU
-#ifndef CONFIG_KPROBES
-extern bool ksu_execveat_hook __read_mostly;
-extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
-				 void *argv, void *envp, int *flags);
-#endif
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
 
-#endif
 
 static void android_service_blacklist(const char *name)
 {
@@ -1762,16 +1755,8 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
-#ifdef CONFIG_KSU
-#ifndef CONFIG_KPROBES
-	if (unlikely(ksu_execveat_hook))
-#endif
-		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-#ifndef CONFIG_KPROBES
-	else
-		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
-#endif
-#endif
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
