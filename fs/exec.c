f0f0f6739186ad769d3d9279460bea7e5a923a64
Neko-Turu <kirmansyah9696@gmail.com>
2023-09-17 06:35:30 +0000

    Revert "treewide: Guard KernelSU changes"
    This reverts commit a36cc1a2eefdeab4a7b0751a60b02dcf5710be91.

--- fs/exec.c
+++ fs/exec.c
@@ -1737,10 +1737,8 @@ static void android_service_blacklist(const char *name)
 	}
 }
 
-#ifdef CONFIG_KSU
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
-#endif
 
 /*
  * sys_execve() executes a new program.
@@ -1755,9 +1753,9 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
-#ifdef CONFIG_KSU
+
 	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-#endif
+
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
