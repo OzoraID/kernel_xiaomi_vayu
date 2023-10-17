f69ba3d4b3dfd1961559b916ab3986067cf95e19
NekoTuru10 <kirmansyah9696@gmail.com>
2023-10-17 15:07:34 +0000

    bruhh

--- drivers/thermal/cpu_cooling.c
+++ drivers/thermal/cpu_cooling.c
@@ -736,7 +736,7 @@ update_frequency:
 	return 0;
 }
 
-void cpufreq_limits_set_level(unsigned int cpu, unsigned int requested)
+void cpu_limits_set_level(unsigned int cpu, unsigned int requested)
 {
 	struct cpufreq_cooling_device *cpufreq_cdev;
 	int i, target;
