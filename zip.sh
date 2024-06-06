dde7ac20592c6555b10cf4a0bc59571bbb30d081
NekoTuru10 <kirmansyah9696@gmail.com>
2024-06-06 19:02:27 +0000

    update script

--- zip.sh
+++ zip.sh
@@ -11,23 +11,35 @@ DEVICE="vayu"
 # Copy Image/dtbo/dtb to AnyKernel3
 for files in {"$KERNEL_IMG","$KERNEL_DTBO","$KERNEL_DTB"}; do
     if [ -f "$files" ]; then
-        echo "* Copy [$files] to AnyKernel3 directory"
+        echo " Copy [$files] to AnyKernel3 directory""
+	sleep 0.5
         if [ "$files" = "$KERNEL_DTB" ]; then
             cp -r "$files" "$AK3_DIR"/dtb.img
+	sleep 0.5
         else
             cp -r "$files" "$AK3_DIR"
+	sleep 0.5
         fi
     else
-        echo "* Image/dtb/dtbo is missing!"
-        echo ""
+	echo -e ""
+        echo -e " Image/dtb/dtbo is missing!"
+        echo -e ""
         exit
     fi
 done
 
 # Compress to ZIP
-echo ""
-echo "* Create ZIP"
+echo -e " Done..."
+echo -e ""
+echo -e " Create ZIP..."
 cd "$AK3_DIR" || exit
 ZIP_NAME=Kurumi+_"$DEVICE"_"$(date +'%d%m%Y')"_"$(date +'%H%M')".zip
 zip -r9 "$ZIP_NAME" ./*
 
+# Upload
+echo -e ""
+echo -e "#####################"
+echo -e "It's Time To Brick.."
+echo -e "#####################"
+echo -e ""
+curl --progress-bar -T "$ZIP_NAME" https://pixeldrain.com/api/file/ | cat || exit
