45ee27c6c8905d88fbe74bf003716311c21d6d69
NekoTuru10 <kirmansyah9696@gmail.com>
2024-06-12 14:11:14 +0000

    update script

--- zip.sh
+++ zip.sh
@@ -9,9 +9,10 @@ AK3_DIR="$KERNEL_DIR/AK3"
 DEVICE="vayu"
 
 # Copy Image/dtbo/dtb to AnyKernel3
+function copy() {
 for files in {"$KERNEL_IMG","$KERNEL_DTBO","$KERNEL_DTB"}; do
     if [ -f "$files" ]; then
-        echo " Copy [$files] to AnyKernel3 directory""
+        echo -e " Copy [$files] to AnyKernel3 directory"
 	sleep 0.5
         if [ "$files" = "$KERNEL_DTB" ]; then
             cp -r "$files" "$AK3_DIR"/dtb.img
@@ -27,19 +28,30 @@ for files in {"$KERNEL_IMG","$KERNEL_DTBO","$KERNEL_DTB"}; do
         exit
     fi
 done
+}
 
-# Compress to ZIP
+# Compress
+function main() {
 echo -e " Done..."
 echo -e ""
 echo -e " Create ZIP..."
 cd "$AK3_DIR" || exit
 ZIP_NAME=Kurumi+_"$DEVICE"_"$(date +'%d%m%Y')"_"$(date +'%H%M')".zip
 zip -r9 "$ZIP_NAME" ./*
+}
 
 # Upload
-echo -e ""
-echo -e "#####################"
-echo -e "It's Time To Brick.."
-echo -e "#####################"
-echo -e ""
+function end() {
+echo  ""
+echo  "#####################"
+echo  "It's Time To Brick  "
+echo  "#####################"
+echo   ""
+
 curl --progress-bar -T "$ZIP_NAME" https://pixeldrain.com/api/file/ | cat || exit
+}
+
+# execute
+copy
+main
+end
